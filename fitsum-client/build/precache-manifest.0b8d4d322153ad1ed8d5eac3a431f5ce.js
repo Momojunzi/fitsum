629cac0dbf17dfae94430cbc886ea7fc08424892
momojunzi <mark.holthaus@gmail.com>
2019-09-20 20:48:48 -0700

    test build 1

---
+++ fitsum-client/build/precache-manifest.0b8d4d322153ad1ed8d5eac3a431f5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6324b74d98cb4fdee3e54723fbff1e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49b03ca0ed0eb92ef22c",
+    "url": "/static/css/main.58c2b1c5.chunk.css"
+  },
+  {
+    "revision": "ef656e7055a3da8c48ac",
+    "url": "/static/js/2.ac956bc9.chunk.js"
+  },
+  {
+    "revision": "49b03ca0ed0eb92ef22c",
+    "url": "/static/js/main.27805231.chunk.js"
+  },
+  {
+    "revision": "7e93513e0f6aca4ef22c",
+    "url": "/static/js/runtime~main.f92bc009.js"
+  }
+]);
